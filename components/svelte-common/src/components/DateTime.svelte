4ee5f3563bbcf723c6d072ff0d26ab4b9f07b143
Markus Felten <Markus.Felten@gmx.de>
2020-07-08 17:27:02 +0200

    fix: loop over components

--- components/svelte-common/src/components/DateTime.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import { dateFormatter } from "../index.mjs";
-  export let date;
-</script>
-
-<time localtime={date.toISOString()}>{dateFormatter.format(date)}</time>
--- components/svelte-common/src/components/Duration.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import { formatDuration, formatDurationISO } from "../index.mjs";
-  export let seconds;
-</script>
-
-<time localtime={formatDurationISO(seconds)}>{formatDuration(seconds)}</time>
